168bb50a3834aa4166bce8d4a85862f548e4ab4f
ImpressionableRaccoon <impressionableracoon@gmail.com>
2023-02-02 03:20:33 +0900

    solve: mutex implementation

--- mutex_impl.go
+++ mutex_impl.go
@@ -1,5 +1,34 @@
 package contest
 
+import (
+	"sync/atomic"
+	"time"
+)
+
+type contextMutex struct {
+	locked atomic.Bool
+}
+
 func New() Mutex {
-	return ... 
+	return &contextMutex{}
+}
+
+func (mu *contextMutex) Lock() {
+	for mu.locked.Load() {
+		time.Sleep(time.Microsecond)
+	}
+	mu.locked.Swap(true)
+}
+
+func (mu *contextMutex) LockChannel() <-chan struct{} {
+	ch := make(chan struct{}, 1)
+	if !mu.locked.Load() {
+		mu.Lock()
+		ch <- struct{}{}
+	}
+	return ch
+}
+
+func (mu *contextMutex) Unlock() {
+	mu.locked.Swap(false)
 }
