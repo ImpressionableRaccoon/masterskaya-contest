b2c0557a8cbf20dedfe5ca2a07b4bd600af80bf4
Vadim Vladimirov <47372390+ImpressionableRaccoon@users.noreply.github.com>
2023-02-03 16:26:52 +0900

    solve
    * solve: mutex implementation
    
    * fix: remove atomic
    
    * fix: use atomic uint32 instead of Bool
    
    * fix: use atomic.CompareAndSwapUint32
    
    * refactor: use channel for contextMutex instead of atomic variable
    
    * refactor: rename contextMutex to contestMutex

--- mutex_impl.go
+++ mutex_impl.go
@@ -1,5 +1,29 @@
 package contest
 
+type contestMutex struct {
+	ch chan struct{}
+}
+
 func New() Mutex {
-	return ... 
+	return &contestMutex{
+		ch: make(chan struct{}, 1),
+	}
+}
+
+func (mu *contestMutex) Lock() {
+	mu.ch <- struct{}{}
+}
+
+func (mu *contestMutex) LockChannel() <-chan struct{} {
+	ch := make(chan struct{}, 1)
+	select {
+	case mu.ch <- struct{}{}:
+		ch <- struct{}{}
+	default:
+	}
+	return ch
+}
+
+func (mu *contestMutex) Unlock() {
+	<-mu.ch
 }
